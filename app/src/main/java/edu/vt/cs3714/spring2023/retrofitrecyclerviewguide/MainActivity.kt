50e7bceaec5e4463e062fef6c5a592275f39161f
kclemmitt <kjclemm03@vt.edu>
2023-07-24 14:51:14 -0400

    Correct Code

--- app/src/main/java/edu/vt/cs3714/spring2023/retrofitrecyclerviewguide/MainActivity.kt
+++ app/src/main/java/edu/vt/cs3714/spring2023/retrofitrecyclerviewguide/MainActivity.kt
@@ -6,7 +6,11 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -16,95 +20,28 @@ import retrofit2.HttpException
 
 class MainActivity : AppCompatActivity() {
 
-    //emulating a repository; this is a list of movieDB movie IDs.
-    private val nowPlaying = mutableListOf(
-        424,
-        11031,
-        8005,
-        154,
-        11,
-        238,
-        426563,
-        463684,
-        505192,
-        568709,
-        491854,
-        398173,
-        445629
-    )
-
-    private val movies = ArrayList<MovieItem>()
     private lateinit var job: Job
-
-    //TODO: add your own key. Hint: https://developers.themoviedb.org/3/getting-started/introduction
-    //Make sure to put your API key in /res/values/strings.xml
-    //Need this to be lazy, because the reference to R.string.api_key doesn't exist until AFTER
-    //onCreate() is executed. By the time we need the apiKey, R.string.api_key exists.
-    private val apiKey by lazy {
-        resources.getString(R.string.api_key)
-    }
-
-    //Creates a handle to the Retrofit Service pointing to the MovieDB URL.
-    private val retrofitService by lazy {
-        RetrofitService.create(resources.getString(R.string.base_url))
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //Do some setup on the recyclerView. Get a handle to the RecyclerView,
-        //create an instance of the adaptor that will provide the list of items
-        //to the recyclerview, and then tell the view to use a linear layout for
-        //the items.
         val recyclerView = findViewById<RecyclerView>(R.id.movie_list)
         val adapter = MovieListAdapter()
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
 
-        //Tell the adaptor about the movie IDs we want
-        adapter.setMovies(movies)
+        val model = ViewModelProvider(this)[MovieViewModel::class.java]
 
-        //Start a job on the IO thread, and fetch each of the movies in turn
-        job = CoroutineScope(Dispatchers.IO).launch {
-
-            //Iterate across the movie ID list
-            for (i: Int in nowPlaying) {
-                //Throtle the calls, so the API doesn't fuss we are making too many, too quickly
-                delay(500)
-
-                //Actually get the record
-                fetchMovieById(i)
-
-                //Notify the RecyclerView adapter that the data has changed. Since the RecyclerView
-                //is on the main/UI thread, we have to set the context to that thread so the adapter
-                //can be notified.
-                withContext(Dispatchers.Main) {
-                    adapter.notifyDataSetChanged()
-
-                }
+        model.allMovies.observe(
+            this,
+            Observer<List<MovieItem>>{ movies ->
+                movies?.let{adapter.setMovies(it)}
             }
-        }
-    }
+        )
 
-    /**
-     * Calls the retrofit service, and gets the record for the specified ID
-     *
-     * @param id MovieId to be fetched.
-     */
-    private suspend fun fetchMovieById(id: Int) {
-        val request = retrofitService.getMovieForId(id, apiKey)
-        try {
-            val movie = request.await()
-
-            movie.let { movies.add(it) }
-
-            Log.d("retrofit_demo", "movie " + id.toString() + " " + movie.toString())
-            // Do something with the response.body()
-        } catch (e: HttpException) {
-            Log.d("retrofit_demo", "HttpException:" + e.toString())
-        } catch (e: Throwable) {
-            Log.d("retrofit_demo", "ThrowableException:" + e.toString())
+        (findViewById<Button>(R.id.refresh)).setOnClickListener{
+            model.refreshMovies(1)
         }
     }
 
@@ -135,7 +72,15 @@ class MainActivity : AppCompatActivity() {
             return movies.size
         }
 
+        /**
+         * Creates the view that will hold the content of an item in the recycler view
+         *
+         * @param parent
+         * @param viewType
+         * @return
+         */
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+            Log.d("T04", "In onCreateViewHolder")
             val v = LayoutInflater.from(parent.context)
                 .inflate(R.layout.card_view, parent, false)
             return MovieViewHolder(v)
@@ -154,18 +99,21 @@ class MainActivity : AppCompatActivity() {
                 movies[position].vote_average.toString()
 
             holder.itemView.setOnClickListener {
-                // interact with the item
+                Log.d("T04", "MovieViewHolder list tap " + holder.view.findViewById<TextView>(R.id.title).text)
             }
         }
 
         inner class MovieViewHolder(val view: View) : RecyclerView.ViewHolder(view),
             View.OnClickListener {
+
             override fun onClick(view: View?) {
-                Log.d("retrofit_demo", "list tap ")
+                Log.d("T04", "list tap in onClick")
                 if (view != null) {
                     //Do some stuff here after the tap
                 }
             }
+
+
 
 
         }
--- app/src/main/java/edu/vt/cs3714/spring2023/retrofitrecyclerviewguide/RetrofitService.kt
+++ app/src/main/java/edu/vt/cs3714/spring2023/retrofitrecyclerviewguide/RetrofitService.kt
@@ -1,12 +1,11 @@
 package edu.vt.cs3714.spring2023.retrofitrecyclerviewguide
 
 import com.google.gson.GsonBuilder
-import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
-import kotlinx.coroutines.Deferred
+import io.reactivex.Observable
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 /**
@@ -15,14 +14,14 @@ import retrofit2.http.Query
  */
 interface  RetrofitService {
 
-    //this annotation maps an API call to the coresponding Kotlin process.
-    @GET("movie/{id}")
-    fun getMovieForId(@Path("id") id: Int, @Query("api_key") api_key: String ): Deferred<MovieItem>
+    //this annotation maps an API call to the corresponding Kotlin process.
+    @GET("movie/now_playing?language=en-US")
+    fun getNowPlaying(@Query("api_key") api_key: String, @Query("page") page: Int ): Observable<Movies>
 
     companion object {
         fun create(baseUrl: String): RetrofitService {
 
-            val retrofit = Retrofit.Builder().addCallAdapterFactory(CoroutineCallAdapterFactory()).addConverterFactory(
+            val retrofit = Retrofit.Builder().addCallAdapterFactory(RxJava2CallAdapterFactory.create()).addConverterFactory(
                 GsonConverterFactory.create(GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss").create()))
                 .baseUrl(baseUrl)
                 .build()
